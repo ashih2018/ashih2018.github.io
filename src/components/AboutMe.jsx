762663bbb8961d57df6c5839e04d65296f772e77
Alex Shih <alex.alex.shih@gmail.com>
2020-04-24 15:31:57 -0400

    updated about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import FunFacts from './FunFacts';
 
 class AboutMe extends Component {
   state = {};
   render() {
     return (
       <div id='about'>
-        I am computer science student studying at University of Toronto.
+        <span>
+          I am computer science student studying at University of Toronto. My
+          hobbies include listening to playing volleyball, listening to music,
+          biking, playing board games, solving Rubik's cubes, and others.
+        </span>
+        <FunFacts></FunFacts>
       </div>
     );
   }
---
+++ src/components/FunFacts.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FunFacts extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        
+      </div>
+     );
+  }
+}
+ 
+export default FunFacts;
