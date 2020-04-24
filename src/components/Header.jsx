37f985b960ebad47dd6c214606daff786d7a1c18
Alex Shih <alex.alex.shih@gmail.com>
2020-04-24 12:50:41 -0400

    created personal website

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from 'react-bootstrap/NavBar'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar>
+          
+        </NavBar>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/HeaderButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HeaderButton extends Component {
+  render() { 
+    return ( 
+      <div>
+        
+      </div>
+     );
+  }
+}
+ 
+export default HeaderButton;
