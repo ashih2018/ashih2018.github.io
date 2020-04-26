951319939307c261d6d3f7acbef4c47eafb978ca
Alex Shih <alex.alex.shih@gmail.com>
2020-04-26 14:24:49 -0400

    updated collapse and panel for experiences tab

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import ExperienceTab from './ExperienceTab';
 import '../styles/Experience.scss';
+import Collapse, { Panel } from 'rc-collapse';
+import 'rc-collapse/assets/index.css';
 
 class Experience extends Component {
-  state = {};
   render() {
     return (
       <div className='experience-wrapper'>
-        <h1>My Experience</h1>
-        <ExperienceTab> </ExperienceTab>
-        <ExperienceTab> </ExperienceTab>
+        <div className='inner-wrapper'>
+          <h1>My Experience</h1>
+          <ExperienceTab></ExperienceTab>
+          <ExperienceTab></ExperienceTab>
+        </div>
       </div>
     );
   }
--- src/components/ExperienceTab.jsx
+++ src/components/ExperienceTab.jsx
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import Expand from 'react-expand-animated';
 import '../styles/Experience.scss';
-
+import Collapse, { Panel } from 'rc-collapse';
 class ExperienceTab extends Component {
-  state = { open: false };
- 
-  toggle = () => {
-    this.setState(prevState => ({ open: !prevState.open }));
-  };
-  
-  render() { 
-    return ( 
-      <div className='tab-wrapper'>
-        <div onClick={this.toggle} className="open">Open</div>
-        <Expand open={this.state.open} className="contents">
-          <div style={{ width: '300px', height: '400px', color: 'red' }}>Hello</div>
-        </Expand>
+  render() {
+    return (
+      <div className='collapse-wrapper'>
+        <Collapse>
+          <Panel className='panel' header={'WatchGuard'}>
+            <p>I did this</p>
+          </Panel>
+        </Collapse>
       </div>
-     );
+    );
   }
 }
- 
+
 export default ExperienceTab;
