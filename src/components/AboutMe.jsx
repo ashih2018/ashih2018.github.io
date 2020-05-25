863d912407634b48ab7d1a4f49fcc002e65ad1ed
Alex Shih <alex.alex.shih@gmail.com>
2020-05-25 14:15:25 -0400

    updated bootstrap components

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 import '../styles/AboutMe.scss';
 import Contact from './Contact';
+import { Container, Row, Col } from 'react-bootstrap'
 
 class AboutMe extends Component {
   state = {};
   render() {
     return (
-      <div id='about' className='section'>
-        {/* <div className='section'> */}
-          <div className='image-container'>
+      <Container id='about'>
+        <Row>
+          <Col sm={4}>
             <img
               className='image'
               src={require('../images/Picture1.jpg')}
-              alt='Picture of me in my apartment!'
+              alt='picture of me in my apartment!'
+              id='mypic'
             />
-          </div>
+          </Col>
 
-          <div className='summary'>
-            <h1 className='title-wrapper'> About Me </h1>
-            I am computer science student studying at University of Toronto. My
+          <Col className='summary' sm={8}>
+            <h1> about me </h1>
+            i am computer science student studying at university of toronto. my
             hobbies include listening to playing volleyball, listening to music,
-            biking, playing board games, solving Rubik's cubes, and others.
-            <Contact />
-          </div>
-        {/* </div> */}
-      </div>
+            biking, playing board games, solving rubik's cubes, and others.
+            <contact />
+          </Col>
+        </Row>
+      </Container>
+      // <div id='about' className='section'>
+      //   {/* <div className='section'> */}
+      //     <div classname='image-container'>
+      //       <img
+      //         classname='image'
+      //         src={require('../images/picture1.jpg')}
+      //         alt='picture of me in my apartment!'
+      //       />
+      //     </div>
+
+      //     <div classname='summary'>
+      //       <h1 classname='title-wrapper'> about me </h1>
+      //       i am computer science student studying at university of toronto. my
+      //       hobbies include listening to playing volleyball, listening to music,
+      //       biking, playing board games, solving rubik's cubes, and others.
+      //       <contact />
+      //     </div>
+      //   {/* </div> */}
+      // </div>
     );
   }
 }
