88f7071823dac869e1e93210a536cee380edfd11
Alex Shih <alex.alex.shih@gmail.com>
2020-06-28 20:12:32 -0400

    updated libaries and other software

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,20 +13,60 @@ const languages = [
   require('../images/skills/languages/Ruby.png'),
 ]
 
+const frameworks = [
+  require('../images/skills/frameworks/Django.png'),
+  require('../images/skills/frameworks/Rails.png'),
+  require('../images/skills/frameworks/React.png'),
+  require('../images/skills/frameworks/Vue.png'),
+  require('../images/skills/frameworks/Flask.png'),
+  require('../images/skills/frameworks/OpenGL.png'),
+]
 
+const other = [
+  require('../images/skills/other/Android.png'),
+  require('../images/skills/other/Git.png'),
+  require('../images/skills/other/Figma.svg'),
+  require('../images/skills/other/Mongo.png'),
+  require('../images/skills/other/Jira.svg'),
+]
 
 class Skills extends Component {
   getLangugages() {
-    const allProjects = [];
+    const allLanguages = [];
     for (let i = 0; i < languages.length; i++) {
-      allProjects.push(
+      allLanguages.push(
         <span>
           <img className='icon' src={languages[i]} />
         </span>
       );
     }
-    return allProjects;
+    return allLanguages;
   }
+
+  getFrameworks() {
+    const allFrameworks = [];
+    for (let i = 0; i < frameworks.length; i++) {
+      allFrameworks.push(
+        <span>
+          <img className='icon' src={frameworks[i]} />
+        </span>
+      );
+    }
+    return allFrameworks;
+  }
+
+  getOther() {
+    const otherSoftware = [];
+    for (let i = 0; i < other.length; i++) {
+      otherSoftware.push(
+        <span>
+          <img className='icon' src={other[i]} />
+        </span>
+      );
+    }
+    return otherSoftware;
+  }
+
   render() {
     return ( 
       <div id='skills' className='section'>
@@ -42,9 +82,13 @@ class Skills extends Component {
               {this.getLangugages()}
             </div>
             <h2> Frameworks and Libraries </h2>
-            <div></div>
+            <div>
+              {this.getFrameworks()}
+            </div>
             <h2> Other Software </h2>
-            <div></div>
+            <div>
+              {this.getOther()}
+            </div>
           </div>
         </div>
       </div>
